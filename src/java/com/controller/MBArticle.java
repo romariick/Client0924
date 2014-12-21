cc61bf74e0afd16c4a3bbbc5701f2cc3aae8e57f
Romaric <Romaric@Romaric-PC>
2014-12-21 01:39:43 -0800

    Initialisationn client.

---
+++ src/java/com/controller/MBArticle.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.controller;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.Serializable;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+/**
+ *
+ * @author Romaric
+ */
+@ViewScoped
+@ManagedBean(name = "MBArticle")
+public class MBArticle implements Serializable {
+
+    @PostConstruct
+    public void init() {
+        try {
+            listerArticle();
+        } catch (IOException ex) {
+            Logger.getLogger(MBArticle.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public void listerArticle() throws MalformedURLException, IOException {
+
+        URL url = new URL("http://localhost:8080/CaisseApplication-war/webresources/listearticle");
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.setRequestProperty("Accept", "application/xml");
+
+        if (conn.getResponseCode() != 200) {
+            throw new RuntimeException("Failed : HTTP error code : "
+                    + conn.getResponseCode());
+        }
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(
+                (conn.getInputStream())));
+
+        String output;
+        System.out.println("Output from Server .... \n");
+        while ((output = br.readLine()) != null) {
+            System.out.println(output);
+        }
+
+        conn.disconnect();
+    }
+}
